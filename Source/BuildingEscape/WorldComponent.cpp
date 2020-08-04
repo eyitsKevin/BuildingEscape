816727135e2073a04807c23a1c0dfb3ac9c284f0
eyitsKevin <kevinateur@hotmail.com>
2020-08-04 16:01:35 -0400

    Dereferencing in order to get name of actor

---
+++ Source/BuildingEscape/WorldComponent.cpp
@@ -0,0 +1,33 @@
+// Copyright Kevin Luu 2020
+
+
+#include "WorldComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldComponent::UWorldComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetName());
+}
+
+
+// Called every frame
+void UWorldComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
