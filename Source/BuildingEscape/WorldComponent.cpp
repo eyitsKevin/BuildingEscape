db374ffb842416165dc52066b0c98cf8b71c36a0
eyitsKevin <kevinateur@hotmail.com>
2021-03-14 22:04:06 -0400

    13.2 - BSP for Basic Building Blocks

--- Source/BuildingEscape/WorldComponent.cpp
+++ Source/BuildingEscape/WorldComponent.cpp
@@ -1,6 +1,5 @@
 // Copyright Kevin Luu 2020
 
-
 #include "WorldComponent.h"
 #include "GameFramework/Actor.h"
 
@@ -10,24 +9,26 @@ UWorldComponent::UWorldComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
-
 
 // Called when the game starts
 void UWorldComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetName());
+
+	const FString ActorName = GetOwner()->GetName();
+	const FVector ActorPosition = GetOwner()->GetActorLocation();
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *ActorName);
+	UE_LOG(LogTemp, Warning, TEXT("It's position is: %s"), *ActorPosition.ToString());
+	UE_LOG(LogTemp, Display, TEXT("Starting the world component"));
 }
 
-
 // Called every frame
-void UWorldComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UWorldComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
 }
-
